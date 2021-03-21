a49630ecddbdd5c240aa8a7000bc2a3260a4b639
tebecheri10 <tebecheri10@gmail.com>
2021-03-20 22:57:42 -0300

    trabajo terminado

--- js/codigo.js
+++ js/codigo.js
@@ -1,13 +1,7 @@
-
 /*var modelo = document.getElementById("etiquetaModelo");
 var precio = document.getElementById("etiquetaValor");*/
 //var resultado = document.getElementById("calcular");
 var costoSeguro = "";
-
-
-
-
-
 
 //------------------------objeto vehiculo----------------->
 function Vehiculo(tipo, anio, valor) {
@@ -22,41 +16,25 @@ function Vehiculo(tipo, anio, valor) {
 
 var vehiculoUno = new Vehiculo("auto", "modelo", "precio");
 
-
-
-
-
-
-
-
 //array para local Storage
 ultimaCotizacion = [];
-
-
-
-
-
-
 
 //evento click para el boton "calcular"
 
 /*resultado.addEventListener("click",*/
 
-    $("#calcular").click (function () {
+$("#calcular").click(function () {
+  var modelo = $("#etiquetaModelo").val(); /* getElement*/
+  var precio = $("#etiquetaValor").val(); /* getElement*/
 
-    var modelo = $("#etiquetaModelo").val();  /* getElement*/
-    var precio = $("#etiquetaValor").val();   /* getElement*/
-
-//funcion para calcular el valor del seguro
-    function costo() {
-      if (precio/*.value*/ <= 100000) {
-        costoSeguro = (precio * 1.5) / 100;
-      } else {
-        costoSeguro = (precio * 2) / 100;
-      }
+  //funcion para calcular el valor del seguro
+  function costo() {
+    if (precio /*.value*/ <= 100000) {
+      costoSeguro = (precio * 1.5) / 100;
+    } else {
+      costoSeguro = (precio * 2) / 100;
     }
-
-
+  }
 
   const combo = document.getElementById("tipo-vehiculo");
   const selected = combo.options[combo.selectedIndex].text;
@@ -72,20 +50,14 @@ ultimaCotizacion = [];
   document.getElementById("modalCosto").innerHTML = `
     <p> Costo de tu vehiculo: $${precio} </p>`;
 
-    document.getElementById("responsabilidad-civil").innerHTML = `
+  document.getElementById("responsabilidad-civil").innerHTML = `
     <h5>$${costoSeguro}</h5> `;
 
-    document.getElementById("terceros-completo").innerHTML = `
-    <h5>$${costoSeguro + (costoSeguro* 10 / 100) }</h5> `;
+  document.getElementById("terceros-completo").innerHTML = `
+    <h5>$${costoSeguro + (costoSeguro * 10) / 100}</h5> `;
 
-    document.getElementById("todo-riesgo").innerHTML = `
-    <h5>$${costoSeguro + (costoSeguro* 30 / 100) }</h5> `;
-
-
-
-  
-    
-
+  document.getElementById("todo-riesgo").innerHTML = `
+    <h5>$${costoSeguro + (costoSeguro * 30) / 100}</h5> `;
 
   //vacia el array para agregar de nuevo con el push
   ultimaCotizacion.splice(0, 3);
@@ -97,19 +69,13 @@ ultimaCotizacion = [];
   //Local Storage primero elimina el anterior y crea uno nuevo
   localStorage.removeItem("ultima cotizacion", utlimaCotizacionString);
   localStorage.setItem("ultima cotizacion", utlimaCotizacionString);
-  ToastUltimaCotizacion()
-
+  ToastUltimaCotizacion();
 });
-
-
-
 
 /*------------mostrar Toast-------------*/
 $(document).ready(function () {
   $(".toast").toast("show");
 });
-
-
 
 //funcion get item para incluir en el Toast
 
@@ -123,83 +89,108 @@ function ToastUltimaCotizacion() {
             <li>Año de vehiculo:${arrayGetLocalStorage[0]}</li>
             <li>Valor del Seguro: $${arrayGetLocalStorage[2]}</li>
            
-       `
-};
-
-
+       `;
+}
 
 //efectos hover en la seleccion de seguro
-$(".card-uno").mouseover(function(){
+$(".card-uno").mouseover(function () {
   $(".card-uno").css("transform", "scale(1.1)");
 });
-$(".card-uno").mouseout(function(){
+$(".card-uno").mouseout(function () {
   $(".card-uno").css("transform", "scale(1)");
 });
-$(".card-dos").mouseover(function(){
+$(".card-dos").mouseover(function () {
   $(".card-dos").css("transform", "scale(1.1)");
 });
-$(".card-dos").mouseout(function(){
+$(".card-dos").mouseout(function () {
   $(".card-dos").css("transform", "scale(1)");
 });
-$(".card-tres").mouseover(function(){
+$(".card-tres").mouseover(function () {
   $(".card-tres").css("transform", "scale(1.1)");
 });
-$(".card-tres").mouseout(function(){
+$(".card-tres").mouseout(function () {
   $(".card-tres").css("transform", "scale(1)");
 });
 
-
 //---------------------------------------------------scroll------
 
-
-$(document).ready(function() {
-  $("#boton-cotizar-automotor").click(function() {
+$(document).ready(function () {
+  $("#boton-cotizar-automotor").click(function () {
     $("body,html").animate(
       {
-        scrollTop: $("#target-cotizador").offset().top
+        scrollTop: $("#target-cotizador").offset().top,
       },
       60 //speed
     );
   });
-  $(".btn-contactar").click(function() {
+  $(".btn-contactar").click(function () {
     $("body,html").animate(
       {
-        scrollTop: $("#target-contacto").offset().top
+        scrollTop: $("#target-contacto").offset().top,
       },
       60 //speed
     );
   });
 });
 
+/*--------------------------boton confirmar solicitud (validar formulario)----*/
+$(".boton-solicitud").click(function () {
+  if (
+    $("#nombreyapellido").val() == 0 ||
+    $("#numeroTelefono").val() == 0 ||
+    $("#email1").val() == 0
+  ) {
+    $(".boton-solicitud").attr("data-target", "");
+  } else {
+    $(".boton-solicitud").attr("data-target", "#modalSolicitud");
+  }
+});
 
 //---------------------------AJAX-----------------------------------
-$(document).ready(function(){
-  
-  $("#boton-cotizar-vida").click(function(){
-    $.get('../sucursales.json', function(datosAsesor){
-         console.log(datosAsesor);
-        
-    $(".modal-asesor-body").html("<ul>"+"<li> nombre: "+datosAsesor.nombre +"</li>"+"<li> sucursal: "+datosAsesor.sucursal +"</li>" +"<li> telefono: "+datosAsesor.telefono +"</li>" +"<li> direccion: "+datosAsesor.direccion +"</li>" +"</ul>")
-         
-    });
+$(document).ready(function () {
+  $("#boton-cotizar-vida").click(function () {
+    $.get("../sucursales.json", function (datosAsesor) {
+      console.log(datosAsesor);
 
+      $(".modal-asesor-body").html(
+        "<ul>" +
+        "<li> nombre: " +
+        datosAsesor.nombre +
+        "</li>" +
+        "<li> sucursal: " +
+        datosAsesor.sucursal +
+        "</li>" +
+        "<li> telefono: " +
+        datosAsesor.telefono +
+        "</li>" +
+        "<li> direccion: " +
+        datosAsesor.direccion +
+        "</li>" +
+        "</ul>"
+      );
+    });
   });
 
+  $("#boton-cotizar-hogar").click(function () {
+    $.get("../sucursales.json", function (datosAsesor) {
+      console.log(datosAsesor);
 
-  $("#boton-cotizar-hogar").click(function(){
-    
-   
-    $.get('../sucursales.json', function(datosAsesor){
-         console.log(datosAsesor);
-        
-    $(".modal-asesor-body").html("<ul>"+"<li> nombre: "+datosAsesor.nombre +"</li>"+"<li> sucursal: "+datosAsesor.sucursal +"</li>" +"<li> telefono: "+datosAsesor.telefono +"</li>" +"<li> direccion: "+datosAsesor.direccion +"</li>" +"</ul>")
-         
+      $(".modal-asesor-body").html(
+        "<ul>" +
+        "<li> nombre: " +
+        datosAsesor.nombre +
+        "</li>" +
+        "<li> sucursal: " +
+        datosAsesor.sucursal +
+        "</li>" +
+        "<li> telefono: " +
+        datosAsesor.telefono +
+        "</li>" +
+        "<li> direccion: " +
+        datosAsesor.direccion +
+        "</li>" +
+        "</ul>"
+      );
     });
-
   });
- 
-
- 
 });
-
-
